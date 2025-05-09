67f9a6d745b673377245bfa8f8e38094f1276118
alperacikbas <acikbas.alper.59@gmail.com>
2025-05-09 12:26:21 +0300

    Uploaded final version

---
+++ ExamDates/ExamDates/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  ExamDates
+//
+//  Created by Trakya9 on 2.05.2025.
+//
+
+import Foundation
+import UIKit
+
+
+class HomeViewController: UIViewController{
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        logoImageView.image = UIImage(named: "logo3")
+    }
+    
+    @IBAction func createExamTapped(_ sender: UIButton){
+        performSegue(withIdentifier: "toCreateExam", sender: self)
+    }
+    
+    @IBAction func viewSavedExamsTapped(_ sender: UIButton){
+        performSegue(withIdentifier: "toSavedExams", sender: self)
+    }
+    
+}
